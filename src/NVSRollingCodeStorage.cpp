51ccb3959d16d4de1605f8ad9168545fb7faa1f3
mensand <andre.mens@bennekens.nl>
2021-10-19 17:25:34 +0200

    inbetween

--- src/NVSRollingCodeStorage.cpp
+++ src/NVSRollingCodeStorage.cpp
@@ -48,4 +48,42 @@ uint16_t NVSRollingCodeStorage::nextCode() {
 	return code;
 }
 
+uint16_t NVSRollingCodeStorage::currentCode() {
+    uint16_t code = 0;
+	esp_err_t err;
+	nvs_handle rcs_handle;
+
+	// Initialize NVS
+	err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		// NVS partition was truncated and needs to be erased
+		// Retry nvs_flash_init
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		err = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(err);
+
+	err = nvs_open(name, NVS_READ, &rcs_handle);
+	ESP_ERROR_CHECK(err);
+
+	err = nvs_get_u16(rcs_handle, key, &code);
+	switch (err) {
+		case ESP_OK:
+			break;
+		case ESP_ERR_NVS_NOT_FOUND:
+			code = 1;
+			break;
+		default:
+			Serial.print("Error reading!");
+			Serial.println(esp_err_to_name(err));
+	}
+	return code;
+}
+
+uint16_t NVSRollingCodeStorage::initCode(uint16_t initialvalue) {
+      return initialvalue;
+}
+
+
+
 #endif
